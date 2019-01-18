27bcb3eda6055c984421f145d9e62b76fd51a4b5
Tori Pope <toricpope@gmail.com>
2019-01-18 11:16:50 -0600

    setup complete

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const layout = require("./views/layout");
 const express = require('express');
 const morgan = require('morgan');
+const { db } = require('./models');
 const app = express();
 
+console.log(db.User);
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+});
 
 app.get('/', (req, res, next) => {
   res.send(layout(''));
-})
+});
 
-app.listen(3000);
+
+const PORT = 3000;
+
+const init = async () => {
+  await db.sync();
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
 
 
 
